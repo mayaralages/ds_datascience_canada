a07788091b7294d562fcce2bb6b2059c070a0cc3
Mayara Lages <mayaralages1@gmail.com>
2021-10-06 21:48:33 -0300

    primeiro passo

---
+++ canada_ds_salary.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+import pandas as pd
+
+path = "C:/Users/mayar/Documents/ds_datascience_canada/Job_list_Canada.csv"
+
+df = pd.read_csv(path)
+
+print(df)
